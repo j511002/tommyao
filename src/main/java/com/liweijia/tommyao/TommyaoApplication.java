7191266ef07ae778e9716884f3ad28efc1afd2c1
jiangZH <jiangizhiheng@liweijia.com>
2017-08-03 23:55:13 +0800

    初始化了项目

---
+++ src/main/java/com/liweijia/tommyao/TommyaoApplication.java
@@ -0,0 +1,12 @@
+package com.liweijia.tommyao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TommyaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TommyaoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/liweijia/tommyao/TommyaoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.liweijia.tommyao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TommyaoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
